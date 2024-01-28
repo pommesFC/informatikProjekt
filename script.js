157759792a75da12f3ab1aecea3c934e6adf82be
Rohit S Bhusal <pommesfc404@gmail.com>
2024-01-28 16:21:39 +0100

    made some changes

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function newPage( buttonId ){
+    switch (buttonId) {
+        case 'grundlagen':
+            window.location.href= 'grundlagen.html';
+            break;
+        case 'hardware':
+            window.location.href= 'hardware.html';
+            break;
+        case 'zahlen':
+            window.location.href= 'zahlen.html';
+            break;
+        case 'vergleichlogik':
+            window.location.href= 'vergleichlogik.html';
+            break;    
+    }
+}
